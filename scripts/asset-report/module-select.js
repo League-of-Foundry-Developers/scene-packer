3743ce69781248cc6b295eb8ddb78b9f75c35f19
Blair <blair@mcmillan.id.au>
2021-05-30 22:04:52 +1000

    v2.2.11
    - Added `Asset Report`.
      - Displays a report showing which assets in your world would not render correctly if they were loaded by someone else via your module.
      - For example, did you miss updating a reference to an asset that exists in your `/worlds` folder?
    - Added new bundled Macros.
      - *Show Asset Report*
        - [View an example video](https://www.youtube.com/watch?v=eBLbUCNfsmk) running against the [Example Scene Packer](https://github.com/sneat/example-scene-packer) module.
      - *Show Scenes Worth Packing*
        - Displays a dialog that lists which Scenes contain data that would benefit from being packed by Scene Packer (e.g. which Scenes have Actors or Journal Pins on them).
    - Fixed incorrectly showing Pack Scene Data context menu option.
      - `Pack Scene Data` context menu wasn't respecting the setting flag correctly. This menu item is once again off by default.

---
+++ scripts/asset-report/module-select.js
@@ -0,0 +1,197 @@
+/**
+ * Module Selection for the Asset Report
+ */
+export default class ModuleSelect extends FormApplication {
+  constructor(...args) {
+    const resolve = args.shift();
+    const reject = args.shift();
+
+    super(...args);
+    this.resolve = resolve;
+    this.reject = reject;
+    this._filter = 'all';
+    this._expanded = false;
+    this._checked = {};
+    this._webExternal = true;
+
+    if (game.modules.get('scene-packer') && ScenePacker?.instances) {
+      Object.keys(ScenePacker.instances).forEach(name => {
+        this._checked[name] = true;
+        // Automatically include dependencies too
+        const module = game.modules.get(name);
+        if (module) {
+          const dependencies = module.data?.dependencies || [];
+          dependencies.forEach(dep => {
+            if (dep?.name) {
+              this._checked[dep.name] = true;
+            }
+          });
+        }
+      });
+      this._checked['scene-packer'] = true;
+    }
+  }
+
+  /** @inheritdoc */
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      title: `${game.i18n.localize('SCENE-PACKER.asset-report.name')} - ${game.i18n.localize('SCENE-PACKER.asset-report.module-select.title')}`,
+      id: 'asset-report-module-select',
+      template: 'modules/scene-packer/templates/asset-report/module-select.html',
+      width: 680,
+      height: 'auto',
+      classes: ['asset-report'],
+      scrollY: ['.package-list'],
+      filters: [{inputSelector: 'input[name="search"]', contentSelector: '.package-list'}],
+    });
+  }
+
+  /** @inheritdoc */
+  getData(options) {
+    // Prepare modules
+    const modules = game.data.modules.reduce((arr, m) => {
+      let mod;
+      if (!isNewerVersion('0.8.0', game.data.version)) {
+        mod = m.data.toObject();
+      } else {
+        mod = duplicate(m.data);
+      }
+      mod.activeModule = m.active;
+      mod.hasPacks = mod.packs.length > 0;
+      mod.hasScripts = mod.scripts.length > 0;
+      mod.hasStyles = mod.styles.length > 0;
+      mod.systemOnly = mod.systems && (mod.systems.indexOf(game.system.id) !== -1);
+      mod.systemTag = game.system.id;
+      mod.incompatible = m.incompatible;
+      mod.unavailable = m.unavailable;
+      mod.dependencies = mod.dependencies ? mod.dependencies.map(d => d.name) : null;
+      return arr.concat([mod]);
+    }, []).sort((a, b) => {
+      if (this._checked[a.name] !== this._checked[b.name]) {
+        return (this._checked[b.name] || 0) - (this._checked[a.name] || 0);
+      }
+      if (a.activeModule !== b.activeModule) {
+        return b.activeModule - a.activeModule;
+      }
+      return a.title.localeCompare(b.title);
+    });
+
+    // Return data for rendering
+    return {modules, expanded: this._expanded, webExternal: this._webExternal};
+  }
+
+
+  /** @inheritdoc */
+  activateListeners(html) {
+    super.activateListeners(html);
+    html.find('button[name="cancel"]').click(this._cancel.bind(this));
+    html.find('.filter').click(this._onFilterList.bind(this));
+    html.find('button.expand').click(this._onExpandCollapse.bind(this));
+    html.find('#scene-packer-module-select-web-external').on('change', this._onChangeWebExternal.bind(this));
+
+    this._restoreCheckboxState();
+  }
+
+  _cancel() {
+    if (typeof this.reject === 'function') {
+      this.reject();
+    }
+    return this.close();
+  }
+
+  /** @inheritdoc */
+  async _updateObject(event, formData) {
+    const selections = [];
+    for (let [k, v] of Object.entries(formData)) {
+      if (!k || !v || k === '__scene-packer-web-external') {
+        continue;
+      }
+      selections.push(k);
+    }
+
+    if (typeof this.resolve === 'function') {
+      this.resolve({selections, webExternal: this._webExternal});
+    }
+  }
+
+  /**
+   * Restores the Form UI to the internal checked state
+   * @private
+   */
+  _restoreCheckboxState() {
+    for (let [k, v] of Object.entries(this._checked)) {
+      if (this.form[k]) {
+        this.form[k].checked = v;
+      }
+    }
+  }
+
+  /**
+   * Handle a button-click to deactivate all modules
+   * @private
+   */
+  _onDeactivateAll(event) {
+    event.preventDefault();
+    for (let input of this.element[0].querySelectorAll('input[type="checkbox"]')) {
+      this._checked[input.name] = input.checked = false;
+    }
+  }
+
+  /**
+   * Handle expanding or collapsing the display of descriptive elements
+   * @private
+   */
+  _onExpandCollapse(event) {
+    event.preventDefault();
+    this._expanded = !this._expanded;
+    this.render();
+  }
+
+  /**
+   * Handle changing whether web assets should be treated as external
+   * @private
+   */
+  _onChangeWebExternal(event) {
+    event.preventDefault();
+    this._webExternal = event.currentTarget.checked;
+  }
+
+  /**
+   * Handle filtering modules
+   * @private
+   */
+  _onFilterList(event) {
+    event.preventDefault();
+    this._filter = event.target.dataset.filter;
+    this.render();
+  }
+
+
+  /** @inheritdoc */
+  _onSearchFilter(event, query, rgx, html) {
+    if (isNewerVersion('0.8.1', game.data.version)) {
+      // rgx and SearchFilter wer added in 0.8.1, previously the arguments were
+      // event, query, html
+      html = rgx;
+      rgx = new RegExp(RegExp.escape(query || ''), 'i');
+    }
+    for (let li of html.children) {
+      if (!query) {
+        li.classList.remove('hidden');
+        continue;
+      }
+      const name = li.dataset.moduleName;
+      const title = (li.querySelector('.package-title')?.textContent || '').trim();
+      const author = (li.querySelector('.author')?.textContent || '').trim();
+      let match = false;
+      if (!isNewerVersion('0.8.1', game.data.version)) {
+        match = rgx.test(SearchFilter.cleanQuery(name)) ||
+          rgx.test(SearchFilter.cleanQuery(title)) ||
+          rgx.test(SearchFilter.cleanQuery(author));
+      } else {
+        match = rgx.test(name) || rgx.test(title) || rgx.test(author);
+      }
+      li.classList.toggle('hidden', !match);
+    }
+  }
+}
