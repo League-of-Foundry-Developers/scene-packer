95ee5bc75c30e7f57945c858ac93fed83cfa028d
Blair McMillan <blair@mcmillan.id.au>
2021-06-17 23:54:43 +1000

    Updated libWrapper shim

--- scripts/shim.js
+++ scripts/shim.js
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 // Copyright © 2021 fvtt-lib-wrapper Rui Pinheiro
 
 
@@ -18,7 +18,7 @@ Hooks.once('init', () => {
 	libWrapper = class {
 		static get is_fallback() { return true };
 
-		static register(module, target, fn, type="MIXED", {chain=undefined}={}) {
+		static register(package_id, target, fn, type="MIXED", {chain=undefined}={}) {
 			const is_setter = target.endsWith('#set');
 			target = !is_setter ? target : target.slice(0, -4);
 			const split = target.split('.');
@@ -34,10 +34,10 @@ Hooks.once('init', () => {
 				if(descriptor) break;
 				iObj = Object.getPrototypeOf(iObj);
 			}
-			if(!descriptor) throw `libWrapper Shim: '${target}' does not exist or could not be found.`;
+			if(!descriptor || descriptor?.configurable === false) throw `libWrapper Shim: '${target}' does not exist, could not be found, or has a non-configurable descriptor.`;
 
 			let original = null;
-			const wrapper = (chain ?? type != 'OVERRIDE') ? function() { return fn.call(this, original.bind(this), ...arguments); } : function() { return fn.call(this, ...arguments); };
+			const wrapper = (chain ?? type != 'OVERRIDE') ? function() { return fn.call(this, original.bind(this), ...arguments); } : function() { return fn.apply(this, arguments); };
 
 			if(!is_setter) {
 				if(descriptor.value) {
@@ -67,24 +67,30 @@ Hooks.once('init', () => {
 	// Set up the ready hook that shows the "libWrapper not installed" warning dialog
 	if(WARN_FALLBACK) {
 		//************** USER CUSTOMIZABLE:
-		// Module ID - by default attempts to auto-detect, but you might want to hardcode your module ID here to avoid potential auto-detect issues
-		const MODULE_ID = ((import.meta?.url ?? Error().stack)?.match(/(?<=\/)modules\/.+(?=\/)/i)??[])[0]?.split('/')?.find(n => n && game.modules.has(n));
-		if(!MODULE_ID) {
-			console.error("libWrapper Shim: Could not auto-detect module ID. The libWrapper fallback warning dialog will be disabled.");
+		// Package ID & Package Title - by default attempts to auto-detect, but you might want to hardcode your package ID and title here to avoid potential auto-detect issues
+		const [PACKAGE_ID, PACKAGE_TITLE] = (()=>{
+			const match = (import.meta?.url ?? Error().stack)?.match(/\/(worlds|systems|modules)\/(.+)(?=\/)/i);
+			if(match?.length !== 3) return [null,null];
+			const dirs = match[2].split('/');
+			if(match[1] === 'worlds') return dirs.find(n => n && game.world.id === n) ? [game.world.id, game.world.title] : [null,null];
+			if(match[1] === 'systems') return dirs.find(n => n && game.system.id === n) ? [game.system.id, game.system.data.title] : [null,null];
+			const id = dirs.find(n => n && game.modules.has(n));
+			return [id, game.modules.get(id)?.data?.title];
+		})();
+
+		if(!PACKAGE_ID || !PACKAGE_TITLE) {
+			console.error("libWrapper Shim: Could not auto-detect package ID and/or title. The libWrapper fallback warning dialog will be disabled.");
 			return;
 		}
 
 		Hooks.once('ready', () => {
-			// Module title
-			const MODULE_TITLE = game.modules.get(MODULE_ID).data.title;
-
 			//************** USER CUSTOMIZABLE:
 			// Title and message for the dialog shown when the real libWrapper is not installed.
-			const FALLBACK_MESSAGE_TITLE = MODULE_TITLE;
+			const FALLBACK_MESSAGE_TITLE = PACKAGE_TITLE;
 			const FALLBACK_MESSAGE = `
-				<p><b>'${MODULE_TITLE}' depends on the 'libWrapper' module, which is not present.</b></p>
+				<p><b>'${PACKAGE_TITLE}' depends on the 'libWrapper' module, which is not present.</b></p>
 				<p>A fallback implementation will be used, which increases the chance of compatibility issues with other modules.</p>
-				<small><p>'libWrapper' is a library which provides module developers with a simple way to modify core Foundry VTT code, while reducing the likelihood of conflict with other modules.</p>
+				<small><p>'libWrapper' is a library which provides package developers with a simple way to modify core Foundry VTT code, while reducing the likelihood of conflict with other packages.</p>
 				<p>You can install it from the "Add-on Modules" tab in the <a href="javascript:game.shutDown()">Foundry VTT Setup</a>, from the <a href="https://foundryvtt.com/packages/lib-wrapper">Foundry VTT package repository</a>, or from <a href="https://github.com/ruipin/fvtt-lib-wrapper/">libWrapper's Github page</a>.</p></small>
 			`;
 
@@ -92,14 +98,14 @@ Hooks.once('init', () => {
 			const DONT_REMIND_AGAIN_KEY = "libwrapper-dont-remind-again";
 
 			// Dialog code
-			console.warn(`${MODULE_TITLE}: libWrapper not present, using fallback implementation.`);
-			game.settings.register(MODULE_ID, DONT_REMIND_AGAIN_KEY, { name: '', default: false, type: Boolean, scope: 'world', config: false });
-			if(game.user.isGM && !game.settings.get(MODULE_ID, DONT_REMIND_AGAIN_KEY)) {
+			console.warn(`${PACKAGE_TITLE}: libWrapper not present, using fallback implementation.`);
+			game.settings.register(PACKAGE_ID, DONT_REMIND_AGAIN_KEY, { name: '', default: false, type: Boolean, scope: 'world', config: false });
+			if(game.user.isGM && !game.settings.get(PACKAGE_ID, DONT_REMIND_AGAIN_KEY)) {
 				new Dialog({
 					title: FALLBACK_MESSAGE_TITLE,
 					content: FALLBACK_MESSAGE, buttons: {
 						ok: { icon: '<i class="fas fa-check"></i>', label: 'Understood' },
-						dont_remind: { icon: '<i class="fas fa-times"></i>', label: "Don't remind me again", callback: () => game.settings.set(MODULE_ID, DONT_REMIND_AGAIN_KEY, true) }
+						dont_remind: { icon: '<i class="fas fa-times"></i>', label: "Don't remind me again", callback: () => game.settings.set(PACKAGE_ID, DONT_REMIND_AGAIN_KEY, true) }
 					}
 				}).render(true);
 			}
