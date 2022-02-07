0c40dcf8fbcae1832c73a841f1f494e6377fc0a2
Blair McMillan <blair@mcmillan.id.au>
2022-02-07 17:19:56 +1000

    Match original actors directly

--- scripts/scene-packer.js
+++ scripts/scene-packer.js
@@ -2663,13 +2663,13 @@ export default class ScenePacker {
       if (CONSTANTS.IsV8orNewer()) {
         matches = game.actors.contents.filter(a => {
           return (
-            a.getFlag(CONSTANTS.MODULE_NAME, 'sourceId') === token.sourceId || a.getFlag('core', 'sourceId') === token.sourceId
+            a.uuid === token.sourceId || a.getFlag(CONSTANTS.MODULE_NAME, 'sourceId') === token.sourceId || a.getFlag('core', 'sourceId') === token.sourceId
           ) && !a.getFlag(CONSTANTS.MODULE_NAME, 'deprecated');
         });
       } else {
         matches = game.actors.entities.filter(a => {
           return (
-            a.getFlag(CONSTANTS.MODULE_NAME, 'sourceId') === token.sourceId || a.getFlag('core', 'sourceId') === token.sourceId
+            a.uuid === token.sourceId || a.getFlag(CONSTANTS.MODULE_NAME, 'sourceId') === token.sourceId || a.getFlag('core', 'sourceId') === token.sourceId
           ) && !a.getFlag(CONSTANTS.MODULE_NAME, 'deprecated');
         });
       }
