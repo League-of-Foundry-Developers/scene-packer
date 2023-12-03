d36d6429fd7a8889bb6e46720fe1c8f4bbc90d3c
Blair <blair@mcmillan.id.au>
2023-12-03 14:01:47 +1000

    v2.7.8
    - Adjustments to handle new data structures expected by [Monk's Active Tile Triggers](https://foundryvtt.com/packages/monks-active-tiles).
      - Thank you NeilWhite for the report.
      - Fixes issue [#136](https://github.com/League-of-Foundry-Developers/scene-packer/issues/136).
    - Fix ownership permission deprecation warning in `toCompendium` method.
    - Updated SVG icons to specify their width/height values to fix compatibility issues with Firefox. Thanks James on Discord for the report.

--- scripts/export-import/related/actor.js
+++ scripts/export-import/related/actor.js
@@ -140,15 +140,24 @@ export function ExtractRelatedTokenAttacherData(actor) {
             return actionData?.macroid ||
               actionData?.entity?.id ||
               actionData?.item?.id ||
+              actionData?.sceneid?.id ||
+              actionData?.sceneid ||
               actionData?.location?.sceneId ||
+              actionData?.rolltableid?.id ||
               actionData?.rolltableid;
           },
         ).forEach(action => {
         const ActionData = CONSTANTS.IsV10orNewer() ? action : action.data;
         if (ActionData?.macroid) {
           relatedData.AddRelation(uuid, {uuid: `Macro.${ActionData.macroid}`, path});
+        } else if (ActionData?.sceneid?.id) {
+          relatedData.AddRelation(uuid, {uuid: `Scene.${ActionData.sceneid.id}`, path});
+        } else if (ActionData?.sceneid) {
+          relatedData.AddRelation(uuid, {uuid: `Scene.${ActionData.sceneid}`, path});
         } else if (ActionData?.location?.sceneId) {
           relatedData.AddRelation(uuid, {uuid: `Scene.${ActionData.location.sceneId}`, path});
+        } else if (ActionData?.rolltableid?.id) {
+          relatedData.AddRelation(uuid, {uuid: `RollTable.${ActionData.rolltableid.id}`, path});
         } else if (ActionData?.rolltableid) {
           relatedData.AddRelation(uuid, {uuid: `RollTable.${ActionData.rolltableid}`, path});
         } else if (ActionData?.entity?.id) {
